af8462f2e26f946156e19689c5874e85717bbeda
seppotk <seppo.kallio2@metropolia.fi>
2022-10-14 03:43:22 +0300

    2.10: The Phonebook Step5 	modified:   src/App.js 	new file:   src/P2_b_forms/components/filter.js 	new file:   src/P2_b_forms/components/personform.js 	new file:   src/P2_b_forms/components/persons.js

--- src/App.js
+++ src/App.js
@@ -333,7 +333,99 @@
 // 2.9*: The Phonebook Step4
 // filter
 
+// import React, { useState } from 'react';
+
+// function App() {
+//   const [ persons, setPersons] = useState([
+//     { name: 'Arto Hellas', number: '040-123456', id: 1 },
+//     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+//     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+//     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+//   ]) 
+//   const [ newName, setNewName ] = useState('')
+//   const [ newNumber, setNewNumber ] = useState('')
+//   const [ filter, setFilter ] = useState('')
+
+//   const addPerson = (event) => {
+//     event.preventDefault()
+//     const personObject = {
+// 	  name: newName,
+// 	  number: newNumber
+//     }
+
+//     console.log(personObject)
+// 	const all_names = persons.map(person => person.name)
+//     if (all_names.includes(newName)) {
+//       alert(`${newName} is already added to phonebook`)
+//       return
+//     }
+//     setPersons(persons.concat(personObject))
+// 	setNewName('')
+// 	setNewNumber('')
+//   }
+
+//   const handleNameChange = (event) => {
+//     console.log(event.target.value)
+//     setNewName(event.target.value)
+//   }
+
+//   const handleNumberChange = (event) => {
+//     setNewNumber(event.target.value)
+//   }
+
+//   const handleFilterChange = (event) => {
+//     setFilter(event.target.value)
+//   }
+//   const personsToShow = filter === ''
+//     ? persons
+//     : persons.filter(person =>
+//     person.name.toLowerCase().includes(filter.toLowerCase()))
+
+//   const row_names = () => personsToShow.map(person => 
+// 	<p key={person.name}>{person.name} {person.number}</p>
+//   )
+
+//   return (
+//     <div>
+//       <h2>Phonebook</h2>
+// 	  <div>
+//         filter shown with <input
+//           value={filter}
+//           onChange={handleFilterChange}
+//         />
+//       </div>
+//       <h2>add a new</h2>
+//       <form onSubmit={addPerson}>
+//         <div>
+//           name: <input
+//             value={newName}
+//             onChange={handleNameChange}
+//           />
+//         </div>
+// 		<div>
+//           number: <input
+//             value={newNumber}
+//             onChange={handleNumberChange}
+//           />
+//         </div>
+//         <div>
+//           <button type="submit">add</button>
+//         </div>
+//       </form>
+//       <h2>Numbers</h2>
+//       <>{row_names()}</>
+//     </div>
+//   )
+// }
+
+// export default App;
+
+// 2.10: The Phonebook Step5
+
 import React, { useState } from 'react';
+import Filter from './P2_b_forms/components/filter';
+import PersonForm from './P2_b_forms/components/personform';
+import Persons from './P2_b_forms/components/persons'
 
 function App() {
   const [ persons, setPersons] = useState([
@@ -388,32 +480,15 @@ function App() {
   return (
     <div>
       <h2>Phonebook</h2>
-	  <div>
-        filter shown with <input
-          value={filter}
-          onChange={handleFilterChange}
-        />
-      </div>
+      <Filter value={filter} onChange={handleFilterChange} />
       <h2>add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input
-            value={newName}
-            onChange={handleNameChange}
-          />
-        </div>
-		<div>
-          number: <input
-            value={newNumber}
-            onChange={handleNumberChange}
-          />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm
+        onSubmit={addPerson}
+        name={{value: newName, onChange: handleNameChange}}
+        number={{value: newNumber, onChange: handleNumberChange}}
+      />
       <h2>Numbers</h2>
-      <>{row_names()}</>
+      <Persons persons={row_names()} />
     </div>
   )
 }
