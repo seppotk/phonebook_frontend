8da7d3286385f17f2048a2acd3d6f7d87a650aca
seppotk <seppo.kallio2@metropolia.fi>
2022-10-16 19:45:48 +0300

    React Part2 C and Myweek41 added 	new file:   .env 	new file:   Myweek41/1-Monday/React_P1_1_Introduction.docx 	new file:   Myweek41/1-Monday/React_P1_2_Javascript.docx 	new file:   Myweek41/2-Tuesday/React_P1_3_Component_state_events.docx 	new file:   Myweek41/2-Tuesday/React_P1_4_More_complex_state_debugging.docx 	new file:   Myweek41/3-Wednesday/CPP_Lecture_6.docx 	new file:   Myweek41/4-Thursday/React_P2_a_rendering_a_collection_modules.docx 	new file:   Myweek41/4-Thursday/React_P2_b_Forms.docx 	new file:   Myweek41/5-Friday/React_P2_c_getting_data_from_server.docx 	new file:   Myweek41/5-Friday/Week41Schoolwork.docx 	new file:   db.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -40,12 +40,32 @@ ReactDOM.createRoot(document.getElementById('root')).render(<App />) */
 
 // this is for part2 
 
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App'
+
+
+
+// ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+
+// this is used for Part 2 c server
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import axios from 'axios'
+
+const promise = axios.get('http://localhost:3001/notes')
+
+promise.then(response => {
+  console.log(response)
+})
+
+
+const promise2 = axios.get('http://localhost:3001/foobar')
+console.log(promise2)
+
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
-
-
