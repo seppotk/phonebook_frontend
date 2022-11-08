2450f68869aecd7235afc1df1f26cacb52bc58a1
seppotk <seppo.kallio2@metropolia.fi>
2022-11-08 14:32:44 +0200

    Part2 note 7 	modified:   db.json 	modified:   src/P2_c_getting_data_from_server/App.js 	modified:   src/P2_c_getting_data_from_server/components/Note.js 	new file:   src/P2_c_getting_data_from_server/components/Notification_note.js 	new file:   src/index.css 	modified:   src/index.js

--- src/P2_c_getting_data_from_server/App.js
+++ src/P2_c_getting_data_from_server/App.js
@@ -1,5 +1,3 @@
-//   export default App
-
 // c notes
 // import { useState } from 'react'
 // import Note from './components/Note'
@@ -174,15 +172,111 @@
 
 // note 2 D version
 
+// import { useState, useEffect } from 'react'
+
+// import Note from './components/Note'
+// import noteService from './services/notes'
+
+// const App = () => {
+//   const [notes, setNotes] = useState([])
+//   const [newNote, setNewNote] = useState('')
+//   const [showAll, setShowAll] = useState(true)
+
+//   useEffect(() => {
+//     noteService
+//       .getAll()
+//       .then(initialNotes => {
+//         setNotes(initialNotes)
+//       })
+//   }, [])
+
+//   const addNote = (event) => {
+//     event.preventDefault()
+//     const noteObject = {
+//       content: newNote,
+//       date: new Date().toISOString(),
+//       important: Math.random() > 0.5,
+//       id: notes.length + 1,
+//     }
+
+//     noteService
+//       .create(noteObject)
+//       .then(returnedNote => {
+//         setNotes(notes.concat(returnedNote))
+//         setNewNote('')
+//       })
+//   }
+
+//   const handleNoteChange = (event) => {
+//     setNewNote(event.target.value)
+//   }
+
+//   const toggleImportanceOf = id => {
+//     const note = notes.find(n => n.id === id)
+//     const changedNote = { ...note, important: !note.important }
+  
+//     noteService
+//       .update(id, changedNote)
+//       .then(returnedNote => {
+//         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+//       })
+//       .catch(error => {
+//         alert(
+//           `the note '${note.content}' was already deleted from server`
+//         )
+//         setNotes(notes.filter(n => n.id !== id))
+//       })
+//   }
+
+//   const notesToShow = showAll
+//     ? notes
+//     : notes.filter(note => note.important)
+
+//   return (
+//     <div>
+//       <h1>Notes</h1>
+//       <div>
+//         <button onClick={() => setShowAll(!showAll)}>
+//           show {showAll ? 'important' : 'all' }
+//         </button>
+//       </div>   
+//       <ul>
+//         {notesToShow.map(note => 
+//           <Note
+//             key={note.id}
+//             note={note}
+//             toggleImportance={() => toggleImportanceOf(note.id)}
+//           />
+//         )}
+//       </ul>
+//       <form onSubmit={addNote}>
+//         <input
+//           value={newNote}
+//           onChange={handleNoteChange}
+//         />
+//         <button type="submit">save</button>
+//       </form>
+//     </div>
+//   )
+// }
+
+// export default App
+
+// --------------------------------------------------------------
+
+// note 2 E version
+
 import { useState, useEffect } from 'react'
 
 import Note from './components/Note'
+import Notification from './components/Notification_note'
 import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     noteService
@@ -223,9 +317,12 @@ const App = () => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
       .catch(error => {
-        alert(
-          `the note '${note.content}' was already deleted from server`
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
         )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
         setNotes(notes.filter(n => n.id !== id))
       })
   }
@@ -237,6 +334,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
--- src/index.js
+++ src/index.js
@@ -60,14 +60,40 @@ ReactDOM.render(<App />, document.getElementById('root')) */
 
 // below for part2 C notes exercises
 
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './P2_c_getting_data_from_server/App'
+// import axios from 'axios'
+
+// axios.get('http://localhost:3001/notes').then(response => {
+//   const notes = response.data
+//   ReactDOM.createRoot(document.getElementById('root')).render(
+//     <App notes={notes} />
+//   )
+// })
+
+
+// below for part2 D notes exercises
+
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './P2_c_getting_data_from_server/App'
+// import axios from 'axios'
+
+
+// axios.get('http://localhost:3001/notes').then(response => {
+//   const notes = response.data
+//   ReactDOM.createRoot(document.getElementById('root')).render(
+//     <App notes={notes} />
+//   )
+// })
+
+// below for part2 E notes exercises
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './P2_c_getting_data_from_server/App'
-import axios from 'axios'
+import './index.css'
 
-axios.get('http://localhost:3001/notes').then(response => {
-  const notes = response.data
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <App notes={notes} />
-  )
-})
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
