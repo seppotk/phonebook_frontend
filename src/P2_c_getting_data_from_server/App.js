52956ece75d3c2eb450aaf3a23046fd637360e65
seppotk <seppo.kallio2@metropolia.fi>
2022-11-08 09:24:04 +0200

    Note part2-5 	modified:   db.json 	modified:   src/P2_c_getting_data_from_server/App.js

--- src/P2_c_getting_data_from_server/App.js
+++ src/P2_c_getting_data_from_server/App.js
@@ -118,72 +118,175 @@
 
 // ----------------------------------------------------------------------
 
-  // 2.4 vrsion
+  // 2.4 version 2 C ending
 
 
-  import { useState, useEffect } from 'react'
-  import axios from 'axios'
-  import Note from './components/Note'
+  // import { useState, useEffect } from 'react'
+  // import axios from 'axios'
+  // import Note from './components/Note'
   
-  const App = () => {
-    const [notes, setNotes] = useState([])
-    const [newNote, setNewNote] = useState('')
-    const [showAll, setShowAll] = useState(true)
+  // const App = () => {
+  //   const [notes, setNotes] = useState([])
+  //   const [newNote, setNewNote] = useState('')
+  //   const [showAll, setShowAll] = useState(true)
   
-    useEffect(() => {
-      axios
-        .get('http://localhost:3001/notes')
-        .then(response => {
-          setNotes(response.data)
-        })
-    }, [])
+  //   useEffect(() => {
+  //     axios
+  //       .get('http://localhost:3001/notes')
+  //       .then(response => {
+  //         setNotes(response.data)
+  //       })
+  //   }, [])
   
-    const addNote = (event) => {
-      event.preventDefault()
-      const noteObject = {
-        content: newNote,
-        date: new Date().toISOString(),
-        important: Math.random() > 0.5,
-        id: notes.length + 1,
-      }
+  //   const addNote = (event) => {
+  //     event.preventDefault()
+  //     const noteObject = {
+  //       content: newNote,
+  //       date: new Date().toISOString(),
+  //       important: Math.random() > 0.5,
+  //       id: notes.length + 1,
+  //     }
   
-      setNotes(notes.concat(noteObject))
-      setNewNote('')
+  //     setNotes(notes.concat(noteObject))
+  //     setNewNote('')
+  //   }
+  
+  //   const handleNoteChange = (event) => {
+  //     console.log(event.target.value)
+  //     setNewNote(event.target.value)
+  //   }
+  
+  //   const notesToShow = showAll
+  //     ? notes
+  //     : notes.filter(note => note.important)
+  
+  //   return (
+  //     <div>
+  //       <h1>Notes</h1>
+  //       <div>
+  //         <button onClick={() => setShowAll(!showAll)}>
+  //           show {showAll ? 'important' : 'all' }
+  //         </button>
+  //       </div>   
+  //       <ul>
+  //         {notesToShow.map(note => 
+  //           <Note key={note.id} note={note} />
+  //         )}
+  //       </ul>
+  //       <form onSubmit={addNote}>
+  //         <input
+  //           value={newNote}
+  //           onChange={handleNoteChange}
+  //         />
+  //         <button type="submit">save</button>
+  //       </form>
+  //     </div>
+  //   )
+  // }
+  
+  // export default App
+
+// --------------------------------------------------------------
+
+// note 2 D version
+
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Note from './components/Note'
+
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        setNotes(response.data)
+      })
+  }, [])
+
+  // const addNote = (event) => {
+  //   event.preventDefault()
+  //   const noteObject = {
+  //     content: newNote,
+  //     date: new Date().toISOString(),
+  //     important: Math.random() > 0.5,
+  //     id: notes.length + 1,
+  //   }
+
+  //   setNotes(notes.concat(noteObject))
+  //   setNewNote('')
+  // }
+
+  // const addNote = event => {
+  //   event.preventDefault()
+  //   const noteObject = {
+  //     content: newNote,
+  //     date: new Date(),
+  //     important: Math.random() < 0.5,
+  //   }
+  
+  //   axios
+  //     .post('http://localhost:3001/notes', noteObject)
+  //     .then(response => {
+  //       console.log(response)
+  //     })
+  // }
+
+
+  const addNote = event => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date(),
+      important: Math.random() > 0.5,
     }
   
-    const handleNoteChange = (event) => {
-      console.log(event.target.value)
-      setNewNote(event.target.value)
-    }
-  
-    const notesToShow = showAll
-      ? notes
-      : notes.filter(note => note.important)
-  
-    return (
-      <div>
-        <h1>Notes</h1>
-        <div>
-          <button onClick={() => setShowAll(!showAll)}>
-            show {showAll ? 'important' : 'all' }
-          </button>
-        </div>   
-        <ul>
-          {notesToShow.map(note => 
-            <Note key={note.id} note={note} />
-          )}
-        </ul>
-        <form onSubmit={addNote}>
-          <input
-            value={newNote}
-            onChange={handleNoteChange}
-          />
-          <button type="submit">save</button>
-        </form>
-      </div>
-    )
+    axios
+      .post('http://localhost:3001/notes', noteObject)
+      .then(response => {
+        setNotes(notes.concat(response.data))
+        setNewNote('')
+      })
   }
+
+
   
-  export default App
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>   
+      <ul>
+        {notesToShow.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
 
 
